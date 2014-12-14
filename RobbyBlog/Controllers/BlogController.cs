e4b6dc13e56eaa9d614793ccd9754c9612670051
sonrobby <sonrobby@hotmail.com>
2014-12-14 23:48:25 +0700

    U

---
+++ RobbyBlog/Controllers/BlogController.cs
@@ -0,0 +1,36 @@
+﻿using RobbyBlog.Core;
+using RobbyBlog.Models;
+using RobbyBlog.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RobbyBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+        //
+        // GET: /Blog/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Posts(int p = 1)
+        {
+            var viewModel = new ListViewModel(_blogRepository, p);
+
+            ViewBag.Title = "Latest Posts";
+            return View("List", viewModel);
+        }
+    }
+}
